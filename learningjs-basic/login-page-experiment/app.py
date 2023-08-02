53e0a4798499ffd56f888fc2ab0814005c31f310
Allen Bercero <abercero@pop-os.localdomain>
2023-08-02 19:22:52 +0800

    updated new exercises and notes

--- learningjs-basic/login-page-experiment/app.py
+++ learningjs-basic/login-page-experiment/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session
 from flask_session import Session
 
 app = Flask(__name__)
@@ -18,11 +18,12 @@ def index():
 def login():
     if request.method == 'POST':
         data = request.get_json()
+        print(data)
         username = data['username']
         password = data['password']
         if username in users and users[username] == password:
             session['username'] = username
-            return 'Login successful'
+            return {'message': 'Login is successful'}, 200
         else:
             return {'message': 'Invalid username or password'}, 401
     else:
